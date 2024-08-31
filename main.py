25b07fe68a0456c26fa7b6856d0fd64d8e1ffd51
oksanabolbat <oksana.bolbat@gmail.com>
2024-08-31 12:04:02 +0200

    fix: refactored

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import pandas as pd
 
-
 df = pd.read_csv("nato_phonetic_alphabet.csv")
 
 alpha_dict = {row.letter: row.code for (ind, row) in df.iterrows()}
 
-is_ready = False
-while not is_ready:
+
+def generate_phonetic():
     user_word = input("please enter a word: ")
     try:
         alphabet_result = [alpha_dict[letter] for letter in user_word.upper()]
         print(alphabet_result)
-        is_ready = True
     except KeyError:
         print("Sorry, only letters in the alphabet please")
+        generate_phonetic()
 
+
+generate_phonetic()
